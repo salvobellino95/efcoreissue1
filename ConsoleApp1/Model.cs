f5858e5ceb426bc3ff9557e5162770790f019c8b
salvobellino95 <salvobellino95@gmail.com>
2020-05-13 16:27:07 +0200

    first commit

---
+++ ConsoleApp1/Model.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFGetStarted
+{
+    public class BloggingContext : DbContext
+    {
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlServer("Server = localhost\\SQLEXPRESS; Database=ConsoleApp1;Trusted_Connection=True;").UseLazyLoadingProxies();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Blog>().HasMany(b => b.Posts).WithOne()
+                .Metadata.PrincipalToDependent.SetPropertyAccessMode(PropertyAccessMode.Field);
+            //modelBuilder.Entity<Blog>().OwnsMany(b => b.Posts).WithOwner(); //this works 
+        }
+    }
+
+    public class Blog
+    {
+        public int Id { get; set; }
+
+        private readonly List<Post> _posts = new List<Post>();
+        public virtual IReadOnlyList<Post> Posts => _posts.ToList();
+
+        public void SetPosts(List<Post> posts)
+        {
+            //some business logic
+            _posts.Clear();
+            _posts.AddRange(posts);
+        }
+    }
+
+    public class Post
+    {
+        public int PostId { get; set; }
+        public string Content { get; set; }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace EFGetStarted
+{
+    class Program
+    {
+        static void Main()
+        {
+            using (var db = new BloggingContext())
+            {
+                db.Database.EnsureDeleted();
+                db.Database.EnsureCreated();
+
+                var blog = new Blog();
+                blog.SetPosts(new List<Post>() { new Post() { Content = "post1" }, new Post() { Content = "post2" } });
+                db.Blogs.Add(blog);
+                db.SaveChanges();
+            }
+
+            using (var db = new BloggingContext())
+            {
+                var blog = db.Blogs.First();
+                blog.SetPosts(new List<Post>() { new Post() { Content = "post3" }, new Post() { Content = "post4" } });
+                db.SaveChanges();
+
+                Debug.Assert(blog.Posts.Count == 2); //fails, it contains also the removed posts post1,post2
+            }
+        }
+    }
+}
